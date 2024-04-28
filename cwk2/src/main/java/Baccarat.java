e8757eeeff7a294dd885f2c9248f091e19b580c9
WillWinterflood <146846548+WillWinterflood@users.noreply.github.com>
2024-04-28 18:25:27 +0000

    hello

--- cwk2/src/main/java/Baccarat.java
+++ cwk2/src/main/java/Baccarat.java
@@ -1,3 +1,48 @@
 public class Baccarat {
   // TODO: Implement your Baccarat simulation program here
+  public static void main (String args[]) {
+    Shoe shoe = new Shoe (6);
+    shoe.shuffle();
+
+    BaccaratHand banker = new BaccaratHand();
+    BaccaratHand player = new BaccaratHand();
+
+    for (int i = 0; i < 2; i++) {
+      player.add(shoe.deal());
+      banker.add(shoe.deal());
+
+    }
+
+    System.out.println("Player: " + player + " = " + player.value());
+    System.out.println("Banker: " + banker + " = " + banker.value());
+
+    if (player.isNatural() == true) {
+      System.out.println("Player has a Natural");
+    }
+    else if (banker.isNatural() == true) {
+      System.out.println("Banker has a Natural");
+    }
+    else if (banker.isNatural() == true && player.isNatural() == true && banker.value() > player.value()) {
+      System.out.println("Both have a Natural. Banker wins");
+    }
+
+    if (banker.value() > player.value()) {
+      System.out.println("Banker Wins!");
+    }
+    else if (player.value() > banker.value()) {
+      System.out.println("Player Wins!");
+    }
+
+    
+
+    System.out.println("Another round? (y/n): ");
+
+    Scanner scanner = new Scanner(System.in);
+    
+
+    
+
+  
+
+  }
 }
